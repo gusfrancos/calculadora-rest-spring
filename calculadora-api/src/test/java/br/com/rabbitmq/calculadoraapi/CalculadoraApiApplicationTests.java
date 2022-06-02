8f8538d4617ab519e64a5a659013731131f2cc0b
gusfrancos <47953550+gusfrancos@users.noreply.github.com>
2022-06-01 21:05:11 -0300

    Estrutura inicial do projeto

---
+++ calculadora-api/src/test/java/br/com/rabbitmq/calculadoraapi/CalculadoraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rabbitmq.calculadoraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/br/com/rabbitmq/calculadora/CalculadoraRestSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.rabbitmq.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraRestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraRestSpringApplication.class, args);
+	}
+
+}
