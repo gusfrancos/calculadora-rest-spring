6f5de3794a787e3c09a34c950e3c318a56eca35c
gusfrancos <gusfrancos@gmail.com>
2022-06-10 16:45:46 -0300

    Update RestApiApplication.java

--- rest-api/src/main/java/br/com/rabbitmq/restapi/RestApiApplication.java
+++ rest-api/src/main/java/br/com/rabbitmq/restapi/RestApiApplication.java
@@ -3,7 +3,9 @@ package br.com.rabbitmq.restapi;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @EnableRabbit
 @SpringBootApplication
 public class RestApiApplication {
