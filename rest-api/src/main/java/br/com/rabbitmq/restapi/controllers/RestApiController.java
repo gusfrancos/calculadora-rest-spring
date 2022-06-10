a98d5a0dcfb9bc5401b59c0f0ef96ec472793d76
gusfrancos <gusfrancos@gmail.com>
2022-06-10 15:02:17 -0300

    Controller somar

--- rest-api/src/main/java/br/com/rabbitmq/restapi/controllers/RestApiController.java
+++ rest-api/src/main/java/br/com/rabbitmq/restapi/controllers/RestApiController.java
@@ -2,6 +2,8 @@ package br.com.rabbitmq.restapi.controllers;
 
 import java.math.BigDecimal;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.rabbitmq.restapi.configs.MDCFilterConfiguration;
 import br.com.rabbitmq.restapi.services.CalculadoraService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "calcular", description = "Endpoint realizações de cálculo")
 @RestController
 @RequestMapping(value = "/calcular/")
 public class RestApiController {
@@ -22,16 +30,22 @@ public class RestApiController {
 	@Autowired
 	private CalculadoraService calculadoraService;
 	
-	@GetMapping(path ="/somar")
-	public ResponseEntity<BigDecimal> somar(@RequestParam("a") BigDecimal a, @RequestParam("b") BigDecimal b)  {
+	@ApiOperation(value = "Operação de soma")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Resultado da soma"),
+			@ApiResponse(code = 404, message = "Operação inválida") })
+	@GetMapping(path ="somar", produces = "application/json", consumes = "application/json")
+	public ResponseEntity<BigDecimal> somar(HttpServletResponse response, @RequestParam("a") BigDecimal a, @RequestParam("b") BigDecimal b)  {
 		LOGGER.info("[Rest API] - method somar [values {} | {}]", a, b);
-		BigDecimal result = calculadoraService.somar(a, b, "teste");
+		BigDecimal result = calculadoraService.somar(a, b, getToken(response));
 		LOGGER.info("[Rest API] - method somar [values {} + {} = {}]", a, b, result);
 		return ResponseEntity.ok(result);
 	}
 	
-	
-
-
-
+	/**
+	 * @param response
+	 * @return
+	 */
+	private String getToken(HttpServletResponse response) {
+		return response.getHeader(MDCFilterConfiguration.DEFAULT_RESPONSE_TOKEN_HEADER);
+	}
 }
--- rest-api/src/test/java/br/com/rabbitmq/restapi/controllers/RestApiControllerTest.java
+++ rest-api/src/test/java/br/com/rabbitmq/restapi/controllers/RestApiControllerTest.java
@@ -1,20 +1,27 @@
 package br.com.rabbitmq.restapi.controllers;
 
+import static org.mockito.ArgumentMatchers.any;
+
 import java.math.BigDecimal;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.boot.devtools.remote.server.Dispatcher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import br.com.rabbitmq.restapi.configs.MDCFilterConfiguration;
 import br.com.rabbitmq.restapi.services.CalculadoraService;
 
 @ExtendWith(SpringExtension.class)
@@ -33,11 +40,11 @@ public class RestApiControllerTest {
 	@Test
 	@DisplayName("Teste da chamada de somar da controller")
 	void list_ReturnsCorrectValueOfSum_WhenSuccessful() {
-		Mockito.when(calculadoraServiceMock.somar(new BigDecimal("1"), new BigDecimal("2"), "teste")).thenReturn(new BigDecimal("3"));
-		ResponseEntity<BigDecimal> resultado = restApiController.somar(new BigDecimal("1"), new BigDecimal("2"));
+	    HttpServletResponse response = Mockito.mock( HttpServletResponse.class);
+	    Mockito.when(response.getHeader(MDCFilterConfiguration.DEFAULT_RESPONSE_TOKEN_HEADER)).thenReturn("teste");
+	    Mockito.when(calculadoraServiceMock.somar(new BigDecimal("1"), new BigDecimal("2"), "teste")).thenReturn(new BigDecimal("3"));
+		ResponseEntity<BigDecimal> resultado = restApiController.somar(response, new BigDecimal("1"), new BigDecimal("2"));
 		Assertions.assertThat(resultado.getBody().intValue()).isNotNull().isEqualTo(3);
 		Assertions.assertThat(resultado.getStatusCode()).isNotNull().isEqualTo(HttpStatus.OK);
-
 	}
-
 }
