e000122beb385e474cc011edc592855114fb84bd
gusfrancos <gusfrancos@gmail.com>
2022-06-30 08:14:20 -0300

    Correçao do docker compose para acesso remoto

--- rest-api/src/main/java/br/com/rabbitmq/restapi/configs/RabbitMQConfiguration.java
+++ rest-api/src/main/java/br/com/rabbitmq/restapi/configs/RabbitMQConfiguration.java
@@ -1,10 +1,14 @@
 package br.com.rabbitmq.restapi.configs;
 
+import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,7 +20,22 @@ public class RabbitMQConfiguration {
 	public static final String ROUTING_KEY = "calculadora-routingkey";
 
 	public static final String QUEUE_NAME = "calculadora-queue";
-
+	
+	
+//	@Bean
+//	public ConnectionFactory connectionFactory() {
+//		CachingConnectionFactory connectionFactory = new CachingConnectionFactory("192.168.1.65");
+//		connectionFactory.setUsername("guest");
+//		connectionFactory.setPassword("guest");
+//		return connectionFactory;
+//	}
+//
+//	@Bean
+//	public AmqpAdmin amqpAdmin() {
+//		return new RabbitAdmin(connectionFactory());
+//	}
+	
+	
 	@Bean
 	Queue messagesQueue() {
 		return QueueBuilder.durable(QUEUE_NAME).build();
