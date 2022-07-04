5ca2e24478b898c068aec38121b04da0807a72e4
gusfrancos <gusfrancos@gmail.com>
2022-07-04 19:53:12 -0300

    Build

--- calculadora-api/src/test/java/br/com/rabbitmq/calculadoraapi/CalculadoraApiApplicationTests.java
+++ calculadora-api/src/test/java/br/com/rabbitmq/calculadoraapi/CalculadoraApiApplicationTests.java
@@ -1,16 +1,6 @@
 package br.com.rabbitmq.calculadoraapi;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-
-
 class CalculadoraApiApplicationTests {
 
-	@Test
-	  void contextLoads(ApplicationContext context) {
-	    assertThat(context).isNotNull();
-	  }
 
 }
